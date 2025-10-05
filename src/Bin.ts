f6bcf3b016e614a1335bc64abee8ed75f3e4aae1
Peter Mandile <peterm1994@hotmail.com>
2025-10-05 13:55:58 +1100

    fixed lids

--- src/Bin.ts
+++ src/Bin.ts
@@ -1,4 +1,4 @@
-import {Entity, MathUtil, RectCollider, RenderCircle, Sprite, VariantSprite} from "lagom-engine";
+import {Entity, MathUtil, RectCollider, Sprite, VariantSprite} from "lagom-engine";
 import {Layers, MainScene} from "./LD58.ts";
 import {Gravity, Phys} from "./Physics.ts";
 import {FlipVals} from "./Flipper.ts";
@@ -7,13 +7,13 @@ import {Mode7Me} from "./Scroller.ts";
 export class BinLid extends Entity {
 
 
-    constructor() {
-        super("binlid", 0, 0, Layers.BIN);
+    constructor(x: number, y: number) {
+        super("binlid", x, y, Layers.BIN);
     }
 
     onAdded() {
         super.onAdded();
-        // this.addComponent(new Mode7Me(this.transform.x, 5));
+        this.addComponent(new Mode7Me(this.transform.x));
         this.addComponent(new VariantSprite(this.scene.game.getResource("bin").textureSliceFromRow(1, 0, 2), {
             xAnchor: 0.5,
             yAnchor: 0.5
@@ -21,15 +21,12 @@ export class BinLid extends Entity {
     }
 }
 
-export class Bin extends Entity
-{
-    constructor(x: number, y: number)
-    {
+export class Bin extends Entity {
+    constructor(x: number, y: number) {
         super("bin", x, y, Layers.BIN);
     }
 
-    onAdded()
-    {
+    onAdded() {
         super.onAdded();
 
         this.addComponent(new Mode7Me(this.transform.x));
@@ -40,7 +37,7 @@ export class Bin extends Entity
             yAnchor: 0.5
         }))
 
-        this.addChild(new BinLid());
+        const lid = this.scene.addEntity(new BinLid(this.transform.x, this.transform.y));
 
         const phys = this.addComponent(new Phys());
         this.addComponent(new RectCollider(MainScene.collSystem, {
@@ -51,19 +48,25 @@ export class Bin extends Entity
             layer: Layers.BIN
         })).onTriggerEnter.register((caller, data) => {
             // console.log(data.result)
-            if (data.other.layer === Layers.FLIPPER)
-            {
+            if (data.other.layer === Layers.FLIPPER) {
                 const flip = data.other.parent.getComponent<FlipVals>(FlipVals);
                 if (flip == null) return;
 
-                phys.yVel = - 75 + -flip.power * 2;
+                phys.yVel = -75 + -flip.power * 2;
                 phys.xVel = MathUtil.randomRange(10, 80) * -flip.side;
                 phys.rot = MathUtil.randomRange(2, 10) * -flip.side;
 
-                if (caller.parent.getComponent(Gravity) == null)
-                {
+                if (caller.parent.getComponent(Gravity) == null) {
                     this.addComponent(new Gravity());
                     this.getComponent(Mode7Me)?.destroy();
+
+                    lid.addComponent(new Gravity());
+                    lid.getComponent(Mode7Me)?.destroy();
+
+                    const lidPhys = lid.addComponent(new Phys());
+                    lidPhys.yVel = -75 + -flip.power * 2.3;
+                    lidPhys.xVel = MathUtil.randomRange(10, 80) * -flip.side;
+                    lidPhys.rot = phys.rot * 0.7
                 }
             }
         });
