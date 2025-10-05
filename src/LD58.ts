4c006c602d367ac9de0ed3e27aa64c44f1a14ed9
Dylan <dylanljenkins@gmail.com>
2025-10-05 14:55:25 +1100

    power bar

--- src/LD58.ts
+++ src/LD58.ts
@@ -25,6 +25,7 @@ import roadLineSpr from "./art/road-line.png";
 import flipper from "./art/flipper.png";
 import signSpr from "./art/sign-roo.png";
 import treeSpr from "./art/tree.png";
+import powerSpr from "./art/power-bar.png";
 import {SoundManager} from "./util/SoundManager";
 import {Truck} from "./Truck.ts";
 import {gravSystem, rotSystem} from "./Physics.ts";
@@ -147,6 +148,7 @@ export class LD58 extends Game
         this.addResource("mute_button", new SpriteSheet(muteButtonSpr, 16, 16));
         this.addResource("bin", new SpriteSheet(binSpr, 13, 17));
         this.addResource("truck", new SpriteSheet(truckSpr, 44, 51))
+        this.addResource("powerbar", new SpriteSheet(powerSpr, 32, 7))
         this.addResource("background", new SpriteSheet(background, 160, 100));
         this.addResource("trees", new SpriteSheet(treeSpr, 50, 44));
         this.addResource("signs", new SpriteSheet(signSpr, 20, 56));
